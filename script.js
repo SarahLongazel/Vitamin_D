78e31786890e37dac695942d5aa00b7ec04569f3
Sarah Longazel <sarah.longazel@gmail.com>
2021-03-21 19:59:20 -0400

    adds preliminary code for D and D in script file, removes attr in answer-dropbox

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // script for drag and drop quiz
+// function onDrop(event) {
+//     const answer = event.dataTransfer.getData("text/plain");
+//     event.target.textContent = answer;
+//     event.preventDefault();
+// }
+
+const dragged;
+document.addEventListener("drag", function( event ) {
+
+}, false);
+
+document.addEventListener("dragstart", function(event) {
+    dragged = event.target;
+}, false);
+
+document.addEventListener("drop", function( event ) {
+    event.preventDefault();
+    
+    if (event.target.className == "answer-dropbox") {
+      dragged.parentNode.removeChild( dragged );
+      event.target.appendChild( dragged );
+    }
+}, false);
